a6c86fff86ce543a95a0838e04a9cbd4bfbdf40e
CatLuck2 <freeaddress834@gmail.com>
2022-04-19 22:50:16 +0900

    Initial Commit

---
+++ NekokichiKadai5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NekokichiKadai5
+//
+//  Created by Nekokichi on 2022/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
