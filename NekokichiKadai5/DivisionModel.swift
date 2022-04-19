e1df29c41658913e423d9aa055c5ef56219700fe
CatLuck2 <freeaddress834@gmail.com>
2022-04-20 00:45:37 +0900

    割り算のモデルを修正

--- NekokichiKadai5/DivisionModel.swift
+++ NekokichiKadai5/DivisionModel.swift
@@ -7,45 +7,28 @@
 
 import UIKit
 
-//struct CalculatableElement {
-//    enum CalculateError: Error {
-//        case EmptyNumeratorError
-//        case EmptyDenominatorError
-//        case InvalidValueError
-//    }
-//    private var Numerator: Int
-//    private var Denominator: Int
-//    func checkElements(closure: (String) -> Void) -> CalculateError {
-//        do {
-//            if Numerator == nil {
-//                throw CalculateError.EmptyNumeratorError
-//            }
-//            if Denominator == nil {
-//                throw CalculateError.EmptyDenominatorError
-//            }
-//            if Numerator == nil {
-//                throw CalculateError.EmptyNumeratorError
-//            }
-//        } catch let error {
-//            switch error {
-//
-//            }
-//        }
-//    }
-//}
-
-protocol Division {
+protocol Calculatable {
     func result() -> Double
 }
 
-class DivisionModel: Division {
-    private var Numerator: Double
-    private var Denominator: Double
-    init(_ numerator: Int, _ denominator: Int) {
-        Numerator = Double(numerator)
-        Denominator = Double(denominator)
+final class Division: Calculatable {
+    private var numerator: Double
+    private var denominator: Double
+    init(_ numeratorValue: Int, _ denominatorValue: Int) {
+        numerator = Double(numeratorValue)
+        denominator = Double(denominatorValue)
     }
     func result() -> Double {
-        Numerator / Denominator
+        numerator / denominator
+    }
+}
+
+final class Calculator {
+    private var method: Calculatable
+    init(_ method: Calculatable) {
+        self.method = method
+    }
+    func result() -> Double {
+        method.result()
     }
 }
