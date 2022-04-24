d635fbe3366943e5e5d20b227e5a2a3456a089c1
akio0911 <akio0911@gmail.com>
2022-04-24 09:21:36 +0900

    feedback

--- NekokichiKadai5/ViewController.swift
+++ NekokichiKadai5/ViewController.swift
@@ -8,18 +8,11 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-    private enum Result<T, Error: Swift.Error> {
-        case success(T)
-        case failure(Error)
-    }
-
     private enum ValueError: Error {
         // 割る数がないエラー
         case emptyNumeratorError
         // 割られる数がないエラー
         case emptyDenominatorError
-        // 割る数が0のエラー
-        case numeratorIsZeroError
         // 割る数が0のエラー
         case denominatorIsZeroError
     }
@@ -28,7 +21,7 @@ final class ViewController: UIViewController {
     @IBOutlet weak private var inputDenominatorField: UITextField!
     @IBOutlet weak private var resultLabel: UILabel!
     // 計算ボタンを押下時に処理を実行するので、初期化はまだ
-    private var calculator: Calculator!
+    private var calculator: Calculatable = Division()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,29 +34,29 @@ final class ViewController: UIViewController {
         switch checkInputValue() {
         case .success:
             // 入力値が有効な数値だと保証されてるので、強制アンラップ
-            let numeratorValue = Int(inputNumeratorField.text!)!
-            let denominatorValue = Int(inputDenominatorField.text!)!
-            calculator = Calculator(Division(numerator: numeratorValue, denominator: denominatorValue))
-            resultLabel.text = "\(calculator.result())"
+            let numeratorValue = Int(inputNumeratorField.text!) ?? 0
+            let denominatorValue = Int(inputDenominatorField.text!) ?? 0
+            resultLabel.text = "\(calculator.result(value1: numeratorValue, value2: denominatorValue))"
         case .failure(.emptyNumeratorError):
-            alert(message: "割る数を入力してください")
-        case .failure(.emptyDenominatorError):
             alert(message: "割られる数を入力してください")
-        case .failure(.numeratorIsZeroError):
-            alert(message: "割る数には0を入力しないでください")
+        case .failure(.emptyDenominatorError):
+            alert(message: "割る数を入力してください")
         case .failure(.denominatorIsZeroError):
-            alert(message: "割られる数には0を入力しないでください")
+            alert(message: "割る数には0を入力しないでください")
         }
     }
 
     private func checkInputValue() -> Result<Void, ValueError> {
         // 入力値が空か
-        if inputNumeratorField.text == "" { return .failure(.emptyNumeratorError) }
-        if inputDenominatorField.text == "" { return .failure(.emptyDenominatorError) }
+        guard !(inputNumeratorField.text ?? "").isEmpty else { return .failure(.emptyNumeratorError) }
+        guard !(inputDenominatorField.text ?? "").isEmpty else { return .failure(.emptyDenominatorError) }
         // 入力値が0か
         // 文字列を入力した場合のエラー処理がわかりませんでした
-        if (Int(inputNumeratorField.text ?? "") ?? 0) == 0 { return .failure(.numeratorIsZeroError) }
-        if (Int(inputDenominatorField.text ?? "") ?? 0) == 0 { return .failure(.denominatorIsZeroError) }
+        guard Int(inputNumeratorField.text ?? "") != nil else { return .failure(.emptyNumeratorError) }
+        guard let denominator = Int(inputDenominatorField.text ?? "") else { return .failure(.emptyDenominatorError) }
+
+        guard denominator != 0 else { return .failure(.denominatorIsZeroError) }
+
         // 成功
         return .success(())
     }
